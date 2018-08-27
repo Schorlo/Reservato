df85c6e9f00498512ccfab7c80564fd2f392baed
Constantin Wischnath <constantinwischnath@hotmail.com>
2018-08-27 17:38:00 +0200

    cards,footer,restaurants_controller,geocoder,maps

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,30 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+
+    @restaurants = Restaurant.where.not(latitude: nil, longitude: nil)
+
+    @markers = @restaurants.map do |restaurant|
+      {
+        lat: restaurant.latitude,
+        lng: restaurant.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+      }
+  end
+
+    def show
+    @restaurant = Restaurant.find(params[:id])
+
+    @markers = [{
+      lat: @restaurant.latitude,
+      lng: @restaurant.longitude
+    }]
+  end
+
+  private
+
+  def reystaurant_params
+    params.require(:restaurant).permit(:category, :name, :city, :category)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_105154) do
+ActiveRecord::Schema.define(version: 2018_08_27_143702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2018_08_27_105154) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "reviews", force: :cascade do |t|

807f111c03b975be5cb170e4b920643276abef59
Nicolas Schorlo <n.vonschorlemer@whu.edu>
2018-08-28 14:34:28 +0200

    cloudinary and images

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -24,7 +24,7 @@ class RestaurantsController < ApplicationController
 
   private
 
-  def reystaurant_params
-    params.require(:restaurant).permit(:category, :name, :city, :category)
+  def restaurant_params
+    params.require(:restaurant).permit(:category, :name, :city, :photo)
   end
 end

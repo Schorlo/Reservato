29a8514cb5d6a89cb99d20ce2686eaa462c7e502
Constantin Wischnath <constantinwischnath@hotmail.com>
2018-08-29 17:37:57 +0200

    Some modification

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :restaurants, only: [:index, :show]
-  resources :reservations,
+  resources :reservations
   resources :reviews
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

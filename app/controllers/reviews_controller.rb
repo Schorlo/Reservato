c3f9819c84e1c0a7ec72cab82afe573efed18b9c
Nicolas Schorlo <n.vonschorlemer@whu.edu>
2018-08-30 16:18:12 +0200

    review function now works perfectly, just needs to be displayed in front end properly

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,6 +5,25 @@ class ReviewsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
 
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.user = current_user
+    if @review.save!
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'restaurants/show'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :restaurants, only: [:index, :show] do
-    resources   :reservations, only: [:index, :new, :create, :destroy]
+    resources :reservations, only: [:index, :new, :create, :destroy]
+    resources :reviews, only: [:index, :show, :new, :create]
   end
 
-  resources :reviews
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
